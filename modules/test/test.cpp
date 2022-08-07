fbd52ac7ecd9d055f7cb85306adc7cfdadd646cd
Hitonoriol <gorgilike@mail.ru>
2022-08-08 00:22:36 +0300

    Update example module

--- modules/test/test.cpp
+++ modules/test/test.cpp
@@ -2,18 +2,29 @@
 
 #include <iostream>
 
-/* Module's entry point */
+/*
+ * Functions can be bound to a methan0l module using `function(name, callable)` or `FUNCTION(name)` macro
+ * Module-local variables can be bound using VAR(name) macro
+ *
+ * Modules can then be loaded and used in multiple ways:
+ *
+ * (1) Loading as a box unit:
+ * 		module = load_module("modules/test") 	(( Only the path to the module's directory is required if its .so file has the same name ))
+ *		<% module.my_sum(11, 42)
+ *
+ * (2) Alternatively, loaded box unit can be imported into the current scope:
+ *		module.import()
+ *		test()
+ *
+ * (3) Or the module itself can be directly imported into the current scope by path:
+ * 		using_module: "$:/modules/test"  (( Assuming the `modules` directory is in the same directory as the interpreter binary ))
+ *		test()
+ */
+
+/* Module's entry point (optional) */
 LOAD_MODULE
 {
-	/*
-	 * Functions defined using `function(name, ...)` overloads will be accessible
-	 * inside of this module's scope, for example:
-	 * 		module = load_module("modules/test/test") 	(( .so extension is optional ))
-	 *		<% module.my_sum(11, 42)
-	 * Alternatively, they can be imported into the current scope, as with any other Unit:
-	 *		module.import()
-	 *		test()
-	 */
+	std::cout << "Loading example module..." << std::endl;
 
 	/* Register function via an InbuiltFunc lambda */
 	function("test", [&](mtl::Args args) {
@@ -26,18 +37,33 @@ LOAD_MODULE
 	
 	/* Set local module variables */
 	VAR(num) = 12345;
+	
+	/* Bind a native class */
+	native_class<TestClass>();
+
+	std::cout << "Finished loading example module:" << std::endl;
+	dump_data_table(*local_scope);
 }
+
+/* Initialize the binding process for `TestClass` */
+NATIVE_CLASS_INIT(TestClass, {
+	auto &binder = get_binder();
+	binder.bind_constructor<std::string&>();
+	binder.bind_method("get_stuff", &THIS_CLASS::get_stuff);
+})
+
 
 int my_sum(int a, int b)
 {
 	return a + b;
 }
 
-void retain_first_word(std::string &str)
+std::string &retain_first_word(std::string &str)
 {
 	auto sep_idx = str.find(' ');
 	if (sep_idx != std::string::npos)
 		str.erase(sep_idx);
+	return str;
 }
 
 /* Another way of hooking functions to the interpreter
@@ -48,10 +74,15 @@ void foo()
 	std::cout << "Function foo()" << std::endl;
 }
 
+void dump_data_table(mtl::DataTable &table)
+{
+	for (auto &&var : *table.map_ptr())
+		std::cout << "  * " << var.first << " (" << var.second.type_name() << ")" << std::endl;
+}
+
 FUNCTION(list_vars)
 void list_vars(mtl::Unit &unit)
 {
-	std::cout << "List of all variables:" << std::endl;
-	for (auto &&var : *unit.local().map_ptr())
-		std::cout << "* " << var.first << " (" << var.second.type_name() << ")" << std::endl;
+	std::cout << "List of all variables of unit:" << std::endl;
+	dump_data_table(unit.local());
 }
--- modules/test/test.h
+++ modules/test/test.h
@@ -4,8 +4,56 @@
 #include <methan0l.h>
 
 int my_sum(int a, int b);
-void retain_first_word(std::string &str);
+std::string &retain_first_word(std::string&);
 void foo();
-void list_vars(mtl::Unit &unit);
+void list_vars(mtl::Unit&);
+void dump_data_table(mtl::DataTable&);
+
+/* Class binding example
+ *
+ * Native classes can be bound to the interpreter either by using:
+ * (1) short-form binding:
+ * 		In header: NATIVE_CLASS(bound_name, native_class_name)
+ *		In source file: 
+ *			NATIVE_CLASS_INIT(bound_name, {
+ *				... // Use get_binder() to get this class's binder instance and bind its constructor and methods using it.
+ *			})
+ *			
+ * In this case `bound_name` and `native_class_name` must be different
+ *  as this implies that class `native_class_name` is already declared.
+ *
+ * (2) or expendable declaration form
+ *  (not really recommended as it inherits NativeClass<> and already has memory overhead of one std::shared_ptr and one reference):
+ * 		In header:
+ *			NATIVE_CLASS_DECL(bound_name, native_class_name)
+ *				... // Declare fields & methods as usual
+ *			NATIVE_CLASS_DECL_END()
+ *		In source file: identical to (1) + define declared methods as usual: `<type> bound_name::method_name(...) {...}`
+ *
+ * Note that classes are registered via the interpreter's type manager globally and are not bound to the module.
+ */
+
+namespace native
+{
+
+class TestClass
+{
+	private:
+		std::string stuff;
+	
+	public:
+		inline TestClass(const std::string &str)
+			: stuff(str) {}
+
+		inline const std::string &get_stuff()
+		{
+			return stuff;
+		}
+};
+
+}
+
+/* You can define a class in a separate namespace to use the same name for bound class */
+NATIVE_CLASS(TestClass, native::TestClass)
 
 #endif /* MODULES_TEST_TEST_H_ */
