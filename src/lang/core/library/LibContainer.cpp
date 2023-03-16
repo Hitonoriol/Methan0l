9997cc4cd1eb3d5df3a7b7f8c3b2a754a1247fba
Hitonoriol <gorgilike@mail.ru>
2023-03-16 11:44:10 +0200

    Register all new container-related class bindings

--- src/lang/core/library/LibContainer.cpp
+++ src/lang/core/library/LibContainer.cpp
@@ -2,7 +2,7 @@
 
 #include <lang/core/library/container/Pair.h>
 #include <lang/core/class/List.h>
-#include <lang/core/class/ListIterator.h>
+#include <lang/core/class/Set.h>
 
 namespace mtl
 {
@@ -11,9 +11,13 @@ METHAN0L_LIBRARY(LibContainer)
 
 void LibContainer::load()
 {
-	context->register_class<Pair>();
-	context->register_class<List>();
-	context->register_class<ListIterator>();
+	REGISTER_METHAN0L_CLASS(*context, Iterable)
+	REGISTER_CLASS_BINDING(*context, Collection)
+	REGISTER_CLASS_BINDING(*context, Pair)
+	REGISTER_CLASS_BINDING(*context, List)
+	REGISTER_CLASS_BINDING(*context, ListIterator)
+	REGISTER_CLASS_BINDING(*context, Set)
+	REGISTER_CLASS_BINDING(*context, SetIterator)
 }
 
 } /* namespace mtl */
