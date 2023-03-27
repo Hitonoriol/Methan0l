00d369a01b74181879fdfdc8081951f1dafd6fe9
Hitonoriol <gorgilike@mail.ru>
2023-03-27 16:23:43 +0300

    Mark all native iterators as implementing `Iterator`

--- src/lang/core/class/DefaultIterator.cpp
+++ src/lang/core/class/DefaultIterator.cpp
@@ -4,10 +4,12 @@ namespace mtl
 {
 
 NATIVE_CLASS_BINDING(ListIterator, {
+	IMPLEMENTS(Iterator)
 	ITERATOR_BINDINGS
 })
 
 NATIVE_CLASS_BINDING(SetIterator, {
+	IMPLEMENTS(Iterator)
 	ITERATOR_BINDINGS
 })
 
