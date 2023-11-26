4a48b67a5ee55a59f1a969bc358690c297e26960
Hitonoriol <hitonoriolx@gmail.com>
2023-11-26 23:31:58 +0200

    Fix compilation error

--- src/core/class/AbstractMap.cpp
+++ src/core/class/AbstractMap.cpp
@@ -1,5 +1,13 @@
 #include "AbstractMap.h"
 
+#include "Collection.h"
+#include "DefaultIterator.h"
+#include "GenericContainer.h"
+
+#include <oop/NativeClass.h>
+#include <structure/Wrapper.h>
+#include <core/Data.h>
+
 namespace mtl
 {
 
