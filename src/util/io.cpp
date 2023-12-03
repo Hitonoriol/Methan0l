fa1c979f85256919473c42425823f8ad20439893
Hitonoriol <hitonoriolx@gmail.com>
2023-12-03 23:12:51 +0200

    Fix file reading

--- src/util/io.cpp
+++ src/util/io.cpp
@@ -11,6 +11,7 @@ std::istream &in = std::cin;
 
 std::string read_file(std::istream &file)
 {
+	file.seekg(0, std::ios::end);
 	auto src_len = file.tellg();
 	file.seekg(std::ios::beg);
 	std::string contents(src_len, 0);
@@ -21,6 +22,10 @@ std::string read_file(std::istream &file)
 std::string read_file(const std::string &name)
 {
 	std::ifstream file(name);
+	if (!file.is_open()) {
+		throw std::runtime_error("Failed to open input file: \"" + name + "\"");
+	}
+	
 	std::string contents = read_file(file);
 	file.close();
 	return contents;
